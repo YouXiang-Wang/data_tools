6a393c4289f6a26f8e3f7401432cc71999fb2ea5
YOUXIANG WANG <youxiang.w@gmail.com>
2024-01-31 12:41:29 -0800

    changes

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,23 @@ use std::io::Write;
 use anyhow::Result;
 use chrono::Utc;
 use clap::Parser;
-use sqlx::{MySql, MySqlPool, Pool};
+use sqlx::{Execute, MySql, MySqlPool, Pool};
 use uuid::Uuid;
 use std::time::{SystemTime, UNIX_EPOCH};
+
+use std::thread::sleep;
+use std::time::Duration;
+
+use sqlx::QueryBuilder;
+use tokio;
+
 /// Simple program to delete records in rocket.messages
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
+
+    #[arg(long, default_value = "insert")]
+    action: String,
 
     #[arg(long, default_value = "127.0.0.1")]
     host: String,
@@ -30,11 +40,14 @@ struct Args {
     #[arg(long, default_value = "t_types_test")]
     table: String,
 
-    #[arg(short, long, default_value_t = 1)]
+    #[arg(short, long, default_value_t = 10000)]
     count: u32,
 
-    #[arg(short, long, default_value_t = 1)]
-    batch: u8
+    #[arg(short, long, default_value_t = 10)]
+    batch: u32,
+
+    #[arg(long, default_value_t = 1000)]
+    rate: i32
 }
 
 
@@ -67,94 +80,133 @@ struct AllTypesTest {
 #[tokio::main]
 async fn main() -> Result<()> {
     let args = Args::parse();
+    let action = &args.action;
+    match action.as_str() {
+        "insert" => {
+            let future = insert_into(&args);
+            future.await?;
+        },
 
-    let host = args.host;
-    let port = args.port;
-    let user = args.user;
-    let password = args.password;
-    let database = args.database;
-    let table = args.table;
-    let count = args.count;
-    let count = args.count;
-    let begin = SystemTime::now()
-        .duration_since(UNIX_EPOCH)
-        .expect("Time went backwards")
-        .as_millis();
+        _ => {
 
-    println!("host={}, port={}, user={}, database={}, table={}, count={}, begin={}", host, port, user, database, table, count, begin);
-
-    let url = format!(
-        "mysql://{}:{}@{}:{}/{}",
-        user, password, host, port , database
-    );
-    let pool = MySqlPool::connect(&url)
-        .await
-        .expect("Failed to connect to MySQL.");
-
-    /*
-    let data = vec![
-        ("1", "1", 1, ""),
-        ("2", "1", 1, "1"),
-        ("3", "1", 1, "1"),
-        ("4", "1", 1, "1")
-
-    ];
-    for row in &data {
-        sqlx::query("INSERT INTO t_0 (id, id1, age, name) VALUES (?, ?, ?, ?)")
-            .bind(row.0)
-            .bind(row.1)
-            .bind(row.2)
-            .bind(row.3)
-            .execute(&pool)
-            .await?;
+        }
     }
-     */
-
-    for _ in 0..count {
-        let id = Uuid::new_v4().to_string();
-        let at = SystemTime::now()
-            .duration_since(UNIX_EPOCH)
-            .expect("Time went backwards")
-            .as_millis();
-
-        println!("Insert into t_types_test. id = {}, at={}", id, at);
-        sqlx::query("INSERT INTO t_types_test(id1, id2, id3, gender, bool_1, bit_1, int_tiny, int_small, int_medium, int_int, int_big, pay1, pay2, pay3, latest_year, latest_date, latest_time, latest_datetime, latest_timestamp, blob_tiny, blob_blob, blob_medium, text1, long_text) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)")
-            .bind(id)
-            .bind("1")
-            .bind("12345678")
-            .bind("male")
-            .bind(true)
-            .bind(1)
-            .bind(123)
-            .bind(12345)
-            .bind(1677215)
-            .bind(26772156)
-            .bind(467721565)
-            .bind(0.99)
-            .bind(0.9999)
-            .bind("3333.50")
-            .bind(2023)
-            .bind("2023-06-16")
-            .bind("12:01:01")
-            .bind("2023-02-16 14:14:14")
-            .bind("2023-06-16 23:59:59")
-            .bind::<Option<Vec<u8>>>(None)
-            .bind::<Option<Vec<u8>>>(None)
-            .bind::<Option<Vec<u8>>>(None)
-            .bind("dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd")
-            .bind("dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd")
-            .execute(&pool)
-            .await?;
-    }
-
-
-    let end = SystemTime::now()
-        .duration_since(UNIX_EPOCH)
-        .expect("Time went backwards")
-        .as_millis();
-    println!("\n===== Insert start_at={}, end_at={}, count={}, cost={}s", begin, end, count, (end - begin) / 1000);
-
     Ok(())
+}
+
+
+
+async fn insert_into(args: &Args) -> Result<()> {
+    let action = &args.action;
+    let table = &args.table;
+    let host = &args.host;
+    let port = &args.port;
+    let user = &args.user;
+    let password = &args.password;
+    let database = &args.database;
+    let count = args.count;
+    let batch = args.batch;
+    let rate = args.rate;
+
+    let _batch = if(rate <= 0) {
+        if(batch > count) {
+            count
+        } else {
+            batch
+        }
+    } else {
+        rate as u32
+    };
+
+    let begin = SystemTime::now().duration_since(UNIX_EPOCH).expect("Time went backwards").as_millis();
+    let url = format!("mysql://{}:{}@{}:{}/{}", user, password, host, port , database);
+    let pool = MySqlPool::connect(&url).await.expect("Failed to connect to MySQL.");
+
+    match table.as_str() {
+        "t_types_test" => {
+            if(count == 0 || batch == 0) {
+                Ok(())
+            } else {
+
+                let round = if(count % _batch == 0) {count / _batch} else {count / _batch + 1};
+                for i in 0..round {
+                    let mut builder = QueryBuilder::new("INSERT INTO t_types_test(id1, id2, id3, gender, bool_1, bit_1, int_tiny, int_small, int_medium, int_int, int_big, pay1, pay2, pay3, latest_year, latest_date, latest_time, latest_datetime, latest_timestamp, blob_tiny, blob_blob, blob_medium, text1, long_text) VALUES ");
+                    let _begin = SystemTime::now().duration_since(UNIX_EPOCH).expect("Time went backwards").as_millis();
+                    for i in 0..batch {
+                        let id = Uuid::new_v4().to_string();
+                        builder.push("(");
+                        builder.push_bind(id);
+                        builder.push(", ");
+                        builder.push_bind("''12345678`~!@#$%^&*()_+-=[]{};:\"'\"',rn.<>?\\|");
+                        builder.push(", ");
+                        builder.push_bind("12345678");
+                        builder.push(", ");
+                        builder.push_bind("male");
+                        builder.push(", ");
+                        builder.push_bind(true);
+                        builder.push(", ");
+                        builder.push_bind(1);
+                        builder.push(", ");
+                        builder.push_bind(123);
+                        builder.push(", ");
+                        builder.push_bind(12345);
+                        builder.push(", ");
+                        builder.push_bind(1677215);
+                        builder.push(", ");
+                        builder.push_bind(26772156);
+                        builder.push(", ");
+                        builder.push_bind(467721565);
+                        builder.push(", ");
+                        builder.push_bind(0.99);
+                        builder.push(", ");
+                        builder.push_bind(0.9999);
+                        builder.push(", ");
+                        builder.push_bind("3333.50");
+                        builder.push(", ");
+                        builder.push_bind(2023);
+                        builder.push(", ");
+                        builder.push_bind("2023-06-16");
+                        builder.push(", ");
+                        builder.push_bind("12:01:01");
+                        builder.push(", ");
+                        builder.push_bind("2023-02-16 14:14:14");
+                        builder.push(", ");
+                        builder.push_bind("2023-06-16 23:59:59");
+                        builder.push(", ");
+                        builder.push_bind::<Option<Vec<u8>>>(None);
+                        builder.push(", ");
+                        builder.push_bind::<Option<Vec<u8>>>(None);
+                        builder.push(", ");
+                        builder.push_bind::<Option<Vec<u8>>>(None);
+                        builder.push(", ");
+                        builder.push_bind("dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd");
+                        builder.push(", ");
+                        builder.push_bind("dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd");
+                        builder.push(")");
+                        if i < batch - 1 {
+                            builder.push(", ");
+                        }
+                    }
+                    let query = builder.build();
+                    query.execute(&pool).await?;
+                    let _end = SystemTime::now().duration_since(UNIX_EPOCH).expect("Time went backwards").as_millis();
+                    let _t = 1000 - (_end - _begin);
+                    if(rate > 0 && _t > 0) {
+                        tokio::time::sleep(tokio::time::Duration::from_millis(_t as u64)).await;
+                    }
+                }
+                let end = SystemTime::now().duration_since(UNIX_EPOCH).expect("Time went backwards").as_millis();
+                println!("action={}, host={}, port={}, user={}, database={}, table={}, count={}, batch={}, expect_rate={}/s, actual_rate={}/s, begin={}, end={}, cost={}s", action, host, port, user, database, table, count, _batch, rate, (count as f32 / ((end  - begin) as f32) as f32 * 1000.00) as u32 , begin, end, ((end  - begin) as f32 / 1000.00));
+                Ok(())
+            }
+        },
+        _ => {
+            println!("t_types_test");
+            Ok(())
+
+        }
+    }
+
 }
 
 fn load_dir(dir: &str) -> Result<Vec<String>> {
@@ -207,5 +259,23 @@ async fn execute_batch_insert(id_file: &str, pool: &Pool<MySql>) -> Result<()> {
         ids.join(", ")
     );
     sqlx::query(&query).execute(pool).await?;
+    Ok(())
+}
+
+
+
+#[tokio::main]
+async fn main1() -> Result<()> {
+    let v1 = test();
+    let v2 =  test();
+    println!("main");
+    tokio::join!(v1, v2);
+    //sleep(Duration::from_secs(1));
+    //tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
+    Ok(())
+}
+async fn test() -> Result<()> {
+    println!("Hello world!");
+    tokio::time::sleep(tokio::time::Duration::from_secs(10)).await;
     Ok(())
 }
