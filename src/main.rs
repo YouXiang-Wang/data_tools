cf583b59241fdaf723527a7b73edf6681862dec3
YOUXIANG WANG <youxiang.w@gmail.com>
2024-02-01 14:38:02 -0800

    changes

--- src/main.rs
+++ src/main.rs
@@ -206,7 +206,7 @@ async fn insert_into(args: &Args) -> Result<()> {
                             Ok(count) => println!("Current count: {}", count),
                             Err(e) => eprintln!("Error querying table count: {}", e),
                         }
-                        sleep(Duration::from_secs(1));
+                        sleep(Duration::from_secs(5));
                     }
                 });
 
